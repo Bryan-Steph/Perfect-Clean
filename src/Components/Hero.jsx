900bac263c1a54a4f80db0501f0015b0361c71cf
Bryan-Steph <frusteveakonnibryan@gmail.com>
2025-03-18 16:07:56 +0100

    Modification of the navbar and background image

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,8 @@ import { useState } from 'react';
   
     return(
         <>
-            <section className='sm:flex block w-full'>
+            <section className='sm:flex block w-full  '>
+                <img src="" alt="" />
                 <div className='sm:w-1/2 w-full'>
             
                     <div className='sm:ml-20 text-center sm:text-start mt-30'>
@@ -25,7 +26,7 @@ import { useState } from 'react';
                         <button className=' h-10 w-25 mt-5 rounded-full  text-white bg-blue-400'>Book Now</button>
                     </div>
                 </div>
-                <div  className='sm:w-1/2 sm:-mt-10 w-full'>
+                <div  className='sm:w-1/2 sm:-mt-10 w-full bg-[url(./assets/images/bg-hero.png)] bg-no-repeat bg-cover'>
                     <img src={heroImage} alt="" />
                 </div>
             </section>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar(){
     }
     return(
         //  <section className='sm:flex block w-full'>
-         <div className='sm:w-1/2 w-full sm:mt-10'>
+         <div className='sm:w-1/2 w-full sm:mt-10 '>
                  <ul className=' sm:flex text-center ml-2  gap-5 sm:ml-20 sm:mt-30 mb-10
                  fixed sm:-top-15 -top-10 backdrop-blur-md bg-white/30 rounded-full shadow-md z-50 mt-20  h-20'>
                         <div className='flex '>
@@ -21,12 +21,12 @@ function Navbar(){
                         {!isMenuOpen ? 
                         <img id='menu-btn' onClick={() => toggleMenu()} className='w-5 h-8 sm:hidden ml-55 m-5 mt-5 ' src={Menu} alt="" />
                         :
-                          <span onClick={() => toggleMenu()} className='w-5  sm:hidden ml-55 m-5 text-4xl  font-bold'>X</span>
+                          <span onClick={() => toggleMenu()} className='w-5  sm:hidden ml-55 m-5 text-4xl  text-blue-600'>X</span>
                       }
                         
                         </div>
                         <div id='main-menu' className={
-                            'flex flex-col md:flex-row gap-6 bg-blue-400 md:bg-transparent rounded-lg items-center  ' + `${isMenuOpen? ' flex' : 'md:flex hidden'}` 
+                            'flex flex-col md:flex-row mt-2 gap-6 backdrop-blur-md bg-white/80 md:bg-transparent rounded-lg items-center  ' + `${isMenuOpen? ' flex' : 'md:flex hidden'}` 
                         }>
                           <li className='font-bold text-xl hover:text-blue-500 hover:transition-colors ' ><a href="">Home</a></li>
                           <li className='font-bold text-xl hover:text-blue-500 hover:transition-colors ' ><a href="">About</a></li>
