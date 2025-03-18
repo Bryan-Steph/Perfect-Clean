4aad32a9387b93366d893d634a763c3aacad5fb3
Bryan-Steph <frusteveakonnibryan@gmail.com>
2025-03-18 17:25:44 +0100

    Adjusting the nav bar humberger overlap left on mobile tryout 2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,13 +15,13 @@ function Navbar(){
         //  <section className='sm:flex block w-full'>
          <div className='sm:w-1/2 w-full sm:mt-10 '>
                  <ul className=' sm:flex text-center ml-2  gap-5 sm:ml-20 sm:mt-30 mb-10
-                 fixed sm:-top-15 -top-10 backdrop-blur-md bg-white/30 rounded-full shadow-md z-50 mt-20  h-20'>
+                 fixed sm:-top-15 -top-10 backdrop-blur-md bg-white/30 rounded-full shadow-md z-50 mt-20 w-full sm:w-1/2  h-20'>
                         <div className='flex '>
                         <img className='sm:mr-20 mr-0 h-10 items-center sm:mt-3 mt-4' src={Logo} alt="" />
                         {!isMenuOpen ? 
                         <img id='menu-btn' onClick={() => toggleMenu()} className='w-5 h-8 sm:hidden ml-40  mt-5 ' src={Menu} alt="" />
                         :
-                          <span onClick={() => toggleMenu()} className='w-5  sm:hidden ml-50  text-4xl  text-blue-600'>X</span>
+                          <span onClick={() => toggleMenu()} className='w-5  sm:hidden ml-40  text-4xl  text-blue-600'>X</span>
                       }
                         
                         </div>
