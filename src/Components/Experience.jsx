185c0413273050f12e1a9e9b5d84391e3400b60b
Bryan-Steph <frusteveakonnibryan@gmail.com>
2025-03-19 04:53:48 +0100

    Adding animations to images

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,4 +1,4 @@
-import check from '../assets/images/check-circle.png'
+import check from '../assets/images/checkblue.png'
 import Eximage from '../assets/images/experience-img.png'
 
 function Experience(){
@@ -9,36 +9,54 @@ function Experience(){
           <div className='sm:flex flex-row-reverse'>
            
             <div className=' sm:w-2/4 mt-20 sm:mr-50'>
-                <h1 className='font-bold text-4xl mx-5 order-1 sm: sm:mx-0'>We are very experienced in cleaning services</h1>
+                <h1 className='font-bold text-4xl mx-5 order-1 sm: sm:mx-0 sm:1/4'>We are very experienced in cleaning services</h1>
                 <p className='mt-5 mx-5 sm:mx-0 w-3/4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                      Vitae eveniet rem cupiditate, alias laudantium corrupti explicabo!</p>
                      <div className='flex mt-3 mx-5 sm:mx-0'> 
                         <img src={check} alt="" />
-                        <h3 className='font-bold ml-2'>We are Commited</h3>
+                        <h3 className='font-bold ml-2 text-2xl'>We are Commited</h3>
                      </div>
-                     <p className='mt-2 mx-5 sm:mx-0'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                     <p className='mt-2 mx-5 sm:mx-0 sm:w-3/4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                          Alias est quia corporis repellendus iure rerum quis.</p>
                          <div className='flex mt-3 mx-5 sm:mx-0'> 
                         <img src={check} alt="" />
-                        <h3 className='font-bold ml-2'>We are Commited</h3>
+                        <h3 className='font-bold ml-2 text-2xl'>We are Commited</h3>
                      </div>
-                     <p className='mt-2 mx-5 sm:mx-0'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                     <p className='mt-2 mx-5 sm:mx-0 sm:w-3/4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                          Alias est quia corporis repellendus iure rerum quis.</p>
                          <div className='flex mt-3 mx-5 sm:mx-0'> 
                         <img src={check} alt="" />
-                        <h3 className='font-bold ml-2'>We are Commited</h3>
+                        <h3 className='font-bold ml-2 text-2xl '>We are Commited</h3>
                      </div>
-                     <p className='mt-2 mx-5 sm:mx-0'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                     <p className='mt-2 mx-5 sm:mx-0 sm:w-3/4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                          Alias est quia corporis repellendus iure rerum quis.</p>
                          <div className='flex mt-3 mx-5 sm:mx-0'> 
                         <img src={check} alt="" />
-                        <h3 className='font-bold ml-2'>We are Commited</h3>
+                        <h3 className='font-bold ml-2 text-2xl'>We are Commited</h3>
                      </div>
-                     <p className='mt-2 mx-5 sm:mx-0'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                     <p className='mt-2 mx-5 sm:mx-0 sm:w-3/4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                          Alias est quia corporis repellendus iure rerum quis.</p>
             </div>
             <div className='mx-0'>
-                <img src={Eximage} className='' />
+            <style>
+        {`
+          @keyframes bounceScale {
+            0%, 100% { transform: scale(1); }
+            50% { transform: scale(1.1); }
+          }
+
+          .desktop-bounce {
+            animation: bounceScale 2.5s ease-in-out infinite;
+          }
+
+          @media (max-width: 768px) {
+            .desktop-bounce {
+              animation: none;
+            }
+          }
+        `}
+      </style>
+                <img src={Eximage} className='md:block  object-cover desktop-bounce' />
             </div>
           </div>
         </>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -16,14 +16,15 @@ import { useState } from 'react';
             
                     <div className='sm:ml-20 text-center sm:text-start mt-30'>
                         <div className='flex mt-10 md:mt-50'> 
-                            <img className='ml-30 sm:ml-0' src={Check} alt="" />
-                            <p className='ml-1 text-white sm:text-black '>ESTABLISHED SINCE 2020</p>
+                            <img className='ml-30 sm:mr-2 sm:ml-0' src={Check} alt="" />
+                            <p className='ml-1 text-white sm:text-gray-400 '>ESTABLISHED SINCE 2020</p>
                         </div>
                     <h1 className='mt-5 font-bold text-5xl capitalize'>  A New solution for Your
                          home cleaning</h1>
                     <p className='text-gray-800 mt-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />
                         Deleniti saepe dignissimos non quia magnam, facere distinctio?</p>
-                        <button className=' h-10 w-50 mt-5 rounded-lg  text-white bg-blue-400'>Book Now</button>
+                        <button className=' h-10 w-50 mt-5 rounded-lg  text-white bg-blue-400 
+                         hover:transition-all hover:bg-blue-700 hover:scale-110'>Book Now</button>
                     </div>
                 </div>
                 <div  className='sm:w-1/2 sm:-mt-10  w-full sm:bg-[url(./assets/images/bg-hero.png)] bg-no-repeat bg-cover'>
